2c5f5f84db2cd09fcd1cf74e264ad9779c6f44ec
Matteo D'apolito <matteodapolito@MacBook-Pro-di-Matteo.fritz.box>
2021-09-03 18:37:11 +0200

    update key db

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, {useEffect} from 'react';
 import Amplify, {API} from 'aws-amplify';
 import config from './aws-exports';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import './App.css';
 
 Amplify.configure(config);
 
 function App() {
 
+  const [userName, setUserName] = React.useState('');
+  const [users, setUsers] = React.useState([]);
+
 
   useEffect(() => {
-    API.get('apiPrenotationSys', '/utenti/nome').then((petRes) => console.log(petRes));
+    API.get('apiPrenotationSys', '/utenti/nome').then((petRes) => {
+      setUsers([...users, ...petRes]);
+      console.log(petRes);
+    });
   }, [])
 
-  const addUser = () => {
-      API.post('apiPrenotationSys', '/utenti/nome', {
-        body: {
-          nome: 'Teo',
-          cognome: 'Dapo'
-        },
-      }).then(() => {
-          console.log("Add success!")
-      });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    API.post('apiPrenotationSys', '/utenti', {
+      body: {
+        nome: userName,
+        cognome: userName,
+      },
+    }).then(() => {
+        setUsers([...users, {nome: userName, cognome: userName}])
+    });
   };
 
   return (
@@ -28,15 +37,19 @@ function App() {
     
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello by matteo
-        </p>
-        <button onClick={addUser}>
-          Add
-        </button>
+        Hello <AmplifySignOut />
+        <form onSubmit={handleSubmit}>
+          <input value={userName} placeholder="Nome" onChange={(e) =>setUserName(e.target.value)}/>
+          <button>Add User</button>
+        </form>
+        <ul>
+          {users.map((user) => (
+            <li>{user.nome}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
